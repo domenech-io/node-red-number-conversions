06a61866edf2a8eacfe2c8cb57e1f49256dead1e
domenech-io <jordi@domenech.io>
2024-08-10 18:25:30 +0200

    correct index.d.ts package name

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "node-red-domenechio-util-fn" {
+declare module "node-red-number-conversions" {
   export const Num: {
       scale: (value: number, min: number, max: number) => number;
       normalize: (value:number, valueMin:number, valueMax:number) => number;
